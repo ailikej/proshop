d5a7bc18ff7883138c19c6d71c5be7b94144e4c4
Eric Jiang <ericjiang1988@gmail.com>
2021-07-22 18:45:04 -0700

    finish section 10 Checkout Process 2

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_SUCCESS,
+    USER_DETAILS_RESET,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -13,7 +14,9 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
+    USER_LIST_RESET,
 } from '../constants/userConstants'
+import { ORDER_CREATE_RESET, ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -52,7 +55,14 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
+    localStorage.removeItem('cartItems')
+    localStorage.removeItem('shippingAddress')
+    localStorage.removeItem('paymentMethod')
     dispatch({ type: USER_LOGOUT })
+    dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
+    dispatch({ type: USER_LIST_RESET })
+    document.location.href = '/login'
 }
 
 export const register = (name, email, password) => async (dispatch) => {
@@ -150,7 +160,7 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
 
         const { data } = await axios.put(`/api/users/profile`, user, config)
 
-        //** to auto update userInfo 
+        //** to auto update userInfo
         dispatch({ type: USER_DETAILS_SUCCESS, payload: data })
         dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
         localStorage.setItem('userInfo', JSON.stringify(data))
