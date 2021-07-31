a93c2344b44bf0945a14f51bf56c8ffe09047e0d
Eric Jiang <ericjiang1988@gmail.com>
2021-07-30 18:20:41 -0700

    fixxx

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            // const conn = await mongoose.connect(
-            //     'mongodb+srv://eric1234:eric1234@cluster0.op9dn.mongodb.net/proshop',
-            //     {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-        })
+        // const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect(
+            'mongodb+srv://eric1234:eric1234@cluster0.op9dn.mongodb.net/proshop?retryWrites=true&w=majority',
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true,
+                useCreateIndex: true,
+            }
+        )
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
